486fbc05e6ed4f36a248ece133df5c63e232b68b
Chisanga Louis Siwale <chisanga.siwale@gmail.com>
2020-04-02 07:50:48 +0200

    Removed PyQt5, django-crispy-forms and django-selectable

--- Hospital HMIS/env/Lib/site-packages/selectable/__init__.py
+++
@@ -1,6 +0,0 @@
-"Auto-complete selection widgets using Django and jQuery UI."
-
-
-__version__ = '1.2.1'
-
-default_app_config = 'selectable.apps.SelectableConfig'
--- Hospital HMIS/env/Lib/site-packages/selectable/tests/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponseNotFound, HttpResponseServerError
-
-
-def test_404(request, *args, **kwargs):
-    return HttpResponseNotFound()
-
-
-def test_500(request, *args, **kwargs):
-    return HttpResponseServerError()
